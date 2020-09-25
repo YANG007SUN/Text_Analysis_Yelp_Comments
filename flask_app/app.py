c9d52000ecc9937fc7df00d31c404a081c22083f
yang sun <yangsun@yangs-MacBook-Pro.local>
2020-09-25 13:01:12 -0700

    update flask

--- flask_app/app.py
+++ flask_app/app.py
@@ -4,14 +4,21 @@ from flask import request
 import sys
 
 
-app = Flask(__name__)
 
+
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    # default summary data
+    summary_data = {
+        "text":"I like orange chicken a lot. Great place to go! Strongly recommended!",
+        "polarity":0.21,
+        "subjecivity":0.81
+    }
+    return render_template("index.html", summary = summary_data)
 
-@app.route("/",methods=["POST"])
+@app.route("/",methods=["POST", "GET"])
 def my_review_value():
     text = request.form["review"]
     summary_data = data_processer.text_summary(text)
